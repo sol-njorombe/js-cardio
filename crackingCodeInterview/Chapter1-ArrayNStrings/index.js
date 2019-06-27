9d2ab6d5690774023ac27f982167e1b56ce2faa0
NJOROMBE <njorombe@gmail.com>
2019-06-27 11:08:05 -0500

    cracking coding interview arrays and hashes

--- crackingCodeInterview/Chapter1-ArrayNStrings/index.js
+++ crackingCodeInterview/Chapter1-ArrayNStrings/index.js
@@ -19,13 +19,12 @@
  *    - Time complexity O(n + n log n)
  *    - Space complexity O(n)
  */
-
 function isUniqueSln1(str) {
   const hash = {};
   for(let char of str){
-    if(hash[char]) {
+    if(hash[char])
       return false;
-    }
+
     hash[char] = 1;
   }
   return true;
@@ -147,7 +146,169 @@ function URLifySln3(str) {
  *
  * APPROACHES
  * ==========
+ * 1. Check if it is an insert/remove. Then use a hash and on str1, fill the hash
+ *    on str2, delete keys from the hash if they exist. Store additional characters of string
+ *    2 in an temp array. By the end, the hash should have at most 1 character and the temp
+ *    array should have at most 1 character.
+ *    - Time complexity O(m + n) sum of length of the two strings
+ *    - Space complexity O(c + n) => O(n) c is constant for the hash. It can only accomodate
+ *      as many characters as there are, n is the length of characters in str2 not in str1
  */
+function oneAway(str1, str2) {
+  let isInsertDel = Math.abs(str1.length - str2.length) === 1;
+  let hash = {};
+  let tempStore = [];
+  for(let char of str1)
+    hash[char] = hash[char] ? hash[char] + 1 : 1;
+
+  for(let char of str2) {
+    if(hash[char] === 1)
+        delete(hash[char]);
+    else if (hash[char] > 1)
+        hash[char]--;
+    else
+      tempStore.push(char);
+  }
+
+  let hashLength = Object.keys(hash).length;
+  if(isInsertDel) {
+    return (hashLength === 1 && tempStore.length === 0) || (hashLength === 0 && tempStore.length === 1);
+  }
+  return (hashLength === 1 && tempStore.length === 1)
+}
+
+/**
+ * stringCompression
+ * Implement a method to perform basic string compression using the counts
+ * of repeated characters. If the "compressing" string is longer than the original
+ * return the original string.
+ * Assumption the string onlty has lower and upper case (a - z)
+ *
+ * APPROACH
+ * ========
+ * 1. On each index compare character to last seen character, if similar increment counter
+ *    otherwise, append character and count to result string, initialize the last seen with the new character
+ *    and set the counter to 1
+ *    - Time complexity O(n)
+ *    - Space complexity O(n) length of result closely resembles the str input
+ */
+function stringCompression(str) {
+  let lastSeen = '';
+  let counter = '';
+  let result = "";
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] === lastSeen) {
+      counter++;
+    }else{
+      result = result + lastSeen + counter;
+      counter = 1;
+      lastSeen = str[i];
+    }
+    if(i === (str.length - 1)) result = result + lastSeen + counter;
+  }
+
+  if(str.length <= result.length) return str;
+  return result;
+}
+
+
+/**
+ * rotateMatrix
+ * Given an image representation by an N x N matrix, where each pixel in
+ * the image is represented by an integer, write a method to rotate the image
+ * by 90 degrees.
+ * You can do this in place
+ *
+ * APPROACHES
+ * ==========
+ * 1. Loop through each row, as you fetch values from the row, prepend the values to each
+ *    row. After all rows are filled, slice out the older values.
+ *    - Time complexity O(n x n) n is sized of the matrix
+ *    - Space complexity O(n) as the values as copied over O(2n) that resolves to O(n)
+ */
+function rotateMatrix(arr) {
+  for(let row = 0; row < arr.length; row++) {
+    for(let j = arr.length; j > 0; j--) {
+      let val = arr[row][arr[row].length - j];
+      arr[arr.length - j].unshift(val);
+    }
+  }
+
+  arr.map(row => row.splice(arr.length, arr.length));
+  return arr;
+}
+
+/**
+ * zeroMatrix
+ * Write an algorithm such that if an element is an M x N matrix is 0, its entire
+ * row and column are set to 0
+ *
+ * APPROACH
+ * ========
+ * 1. Traverse the array and collect all the rows with zeros and all the columns with zeros
+ *    Then start by converting the values in the rows to zeros, then for the rows not touched
+ *    but still have columns with zeros, convert the column values to zeros
+ *    - Time complexity O(n x m) n and m are the dimensions of the array
+ *    - Space complexity O(n + m) The hashes can only store a maximum of the number of cols and rows
+ */
+function zeroMatrix(arr) {
+  let zeroRows = {};
+  let zeroCols = {};
+  let allRows = {};
+  for(let row = 0; row < arr.length; row++) {
+    allRows[row] = true;
+    for(let col = 0; col < arr[row].length; col++) {
+      if(arr[row][col] === 0) {
+        zeroRows[row] = true;
+        zeroCols[col] = true;
+      }
+    }
+  }
+
+  for(let row of Object.keys(zeroRows)) {
+    delete(allRows[row]);
+    for(let i = 0; i < arr[row].length; i++) {
+      arr[row][i] = 0;
+    }
+  }
+
+  for(let col of Object.keys(zeroCols)) {
+    for(let row of Object.keys(allRows)) {
+      arr[row][col] = 0;
+    }
+  }
+
+  return arr;
+}
+
+/**
+ * stringRotation
+ * Assume you have a method isSubstring which checks if one string is a substring of another
+ * Given two strings s1 and s2 write code to check if s2 is a rotation of s1 using only
+ * one call to isSubstring
+ *
+ * APPROACH
+ * ========
+ * 1. Find the possible beginning of the the second string in reference to the first string
+ *    then reorganize the second string to start from the possible begining and test whether
+ *    the resulting string is a substring
+ *    - Time complexity O(n). We are iterating ove s2
+ *    - Space complexity O(n). The substr will be a copy of s2
+ */
+function stringRotation(s1, s2) {
+  for(let i = 0; i < s2.length; i++) {
+    if(s2[i] === s1[0] && s2.slice(i, s2.length) === s1.slice(0, (s2.length - i))) {
+      let substr = s2.slice(i, s2.length) + s2.slice(0, i);
+      return isSubstring(s1, substr);
+    }
+  }
+  return false;
+}
+
+function isSubstring(s1, s2) {
+  let regex = RegExp(s2);
+  return regex.test(s1);
+}
 
 module.exports = {
   isUniqueSln1,
@@ -156,5 +317,11 @@ module.exports = {
   isPermutationSln2,
   URLifySln1,
   URLifySln2,
-  URLifySln3
+  URLifySln3,
+  oneAway,
+  stringCompression,
+  rotateMatrix,
+  zeroMatrix,
+  stringRotation,
+  isSubstring
 }
--- crackingCodeInterview/Chapter1-ArrayNStrings/test.js
+++ crackingCodeInterview/Chapter1-ArrayNStrings/test.js
@@ -8,7 +8,7 @@ describe('Test that a strng has unique characters', () => {
   test('Solution 2', () => {
     expect(slns.isUniqueSln2('test')).toEqual(false);
     expect(slns.isUniqueSln2('carot')).toEqual(true);
-  })
+  });
 });
 
 describe('Given two strings are they permutations of each other', () => {
@@ -19,7 +19,7 @@ describe('Given two strings are they permutations of each other', () => {
   test('Solution 2: Sort String', () => {
     expect(slns.isPermutationSln2('cat', 'tac')).toEqual(true);
     expect(slns.isPermutationSln2('cow', 'pig')).toEqual(false);
-  })
+  });
 });
 
 describe('Given a string change the spaces into \'%20\' like a URL format', () => {
@@ -31,5 +31,50 @@ describe('Given a string change the spaces into \'%20\' like a URL format', () =
   });
   test('Solution 2: Regex', () => {
     expect(slns.URLifySln3('Mr John Smith    ')).toEqual('Mr%20John%20Smith');
+  });
+});
+
+describe('Determine whether the second string is one edit away', () => {
+  test('oneAway', () => {
+    expect(slns.oneAway('pale', 'ple')).toEqual(true);
+    expect(slns.oneAway('pales', 'pale')).toEqual(true);
+    expect(slns.oneAway('pale', 'bale')).toEqual(true);
+    expect(slns.oneAway('pale', 'bake')).toEqual(false);
+    expect(slns.oneAway('cool', 'col')).toEqual(true);
+  });
+});
+
+describe('String compression', () => {
+  test('stringCompression function', () => {
+    expect(slns.stringCompression('aabcccccaaa')).toEqual('a2b1c5a3');
+    expect(slns.stringCompression('aabbc')).toEqual('aabbc');
+    expect(slns.stringCompression('aaaaaaaaaaa')).toEqual('a11');
+  });
+});
+
+describe('Rotate matrix like an image', () => {
+  test('image rotation function', () => {
+    expect(slns.rotateMatrix([[1,2,3], [4,5,6], [7,8,9]])).toEqual([[7, 4, 1],[8,5,2], [9,6,3]])
+    expect(slns.rotateMatrix([[1]])).toEqual([[1]]);
+    expect(slns.rotateMatrix([[5,6], [8,9]])).toEqual([[8,5], [9,6]]);
+  });
+});
+
+describe('true row and columns with zeros to all zeros', () => {
+  test('zeroMatrix function', () => {
+    expect(slns.zeroMatrix([[1,2,0,4], [5,6,7,8], [0, 10, 11, 12]])).toEqual([[0,0,0,0], [0,6,0,8], [0,0,0,0]]);
+  });
+});
+
+describe('Determine if s2 is a rotation of s1, by one call to isSubstring', () => {
+  test('isSubstring', () => {
+    expect(slns.isSubstring('Survive', 'vive')).toEqual(true);
+    expect(slns.isSubstring('subtract', 'tract')).toEqual(true);
+    expect(slns.isSubstring('subtract', 'traction')).toEqual(false);
+  });
+
+  test('stringRotation', () => {
+    expect(slns.stringRotation('waterbottle', 'erbottlewat')).toEqual(true);
+    expect(slns.stringRotation('waterbottle', 'erbottewat')).toEqual(false);
   });
 })
